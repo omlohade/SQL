33e27a6182a1ebbf814f7de78420a0232cdb7bdc
omlohade <omlohade2@gmail.com>
2025-01-23 04:34:36 +0530

    easy

---
+++ easy/619.BigestSingleNumber.sql
@@ -0,0 +1,80 @@
+-- Table: MyNumbers
+
+-- +-------------+------+
+-- | Column Name | Type |
+-- +-------------+------+
+-- | num         | int  |
+-- +-------------+------+
+-- This table may contain duplicates (In other words, there is no primary key for this table in SQL).
+-- Each row of this table contains an integer.
+ 
+
+-- A single number is a number that appeared only once in the MyNumbers table.
+
+-- Find the largest single number. If there is no single number, report null.
+
+-- The result format is in the following example.
+
+ 
+
+-- Example 1:
+
+-- Input: 
+-- MyNumbers table:
+-- +-----+
+-- | num |
+-- +-----+
+-- | 8   |
+-- | 8   |
+-- | 3   |
+-- | 3   |
+-- | 1   |
+-- | 4   |
+-- | 5   |
+-- | 6   |
+-- +-----+
+-- Output: 
+-- +-----+
+-- | num |
+-- +-----+
+-- | 6   |
+-- +-----+
+-- Explanation: The single numbers are 1, 4, 5, and 6.
+-- Since 6 is the largest single number, we return it.
+-- Example 2:
+
+-- Input: 
+-- MyNumbers table:
+-- +-----+
+-- | num |
+-- +-----+
+-- | 8   |
+-- | 8   |
+-- | 7   |
+-- | 7   |
+-- | 3   |
+-- | 3   |
+-- | 3   |
+-- +-----+
+-- Output: 
+-- +------+
+-- | num  |
+-- +------+
+-- | null |
+-- +------+
+-- Explanation: There are no single numbers in the input table so we return null.
+
+
+
+
+SELECT MAX(a.num) AS num
+FROM (
+    -- Subquery: Selects unique numbers from MyNumbers
+    select num 
+    from MyNumbers 
+    group by num
+    having count(num) = 1  -- Filters to only include numbers that appear once
+) a
+
+
+--a(any name) is compalsary 
